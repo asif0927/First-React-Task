81940f939d363fe20d580385c3772842a4cb31ab
asif0927 <6zd6jws@code.edu.az>
2023-05-06 02:17:21 +0400

    first task

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header  from './Components/Header/Header';
+import About from './Components/About/About';
+import Footer  from './Components/Footer/Footer';
+import Contact from './Components/Contact/Contact';
+import Products from './Components/Products/Products';
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <About></About>
+    <Contact></Contact>
+    <Products></Products>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
